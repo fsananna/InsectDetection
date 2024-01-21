27b6b8503f24790632f0371aab946bb52590fac4
fsananna <sultanafarhana55@gmail.com>
2024-01-21 20:49:48 +0600

    Error

--- app/src/main/java/com/example/insectdetection/Register.java
+++ app/src/main/java/com/example/insectdetection/Register.java
@@ -85,6 +85,9 @@ textView.setOnClickListener(new View.OnClickListener() {
 
                                     Toast.makeText(Register.this, "Account Created.",
                                             Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
 
                                 } else {
                                     // If sign in fails, display a message to the user.
