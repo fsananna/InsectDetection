08ae7e142e1fe280fbdbe469efe48f74a1589f63
fsananna <sultanafarhana55@gmail.com>
2024-01-21 22:25:39 +0600

    added login backend

--- app/src/main/java/com/example/insectdetection/Register.java
+++ app/src/main/java/com/example/insectdetection/Register.java
@@ -12,6 +12,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,7 +26,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
 
-    TextInputEditText editTextEmail, editTextPassword;
+    EditText editTextEmail, editTextPassword;
     Button buttonReg;
     FirebaseAuth mAuth;
     ProgressBar progressBar;
