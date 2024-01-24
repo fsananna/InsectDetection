123b0eacaf72ca097d3aaaba098791ac7b20572c
fsananna <sultanafarhana55@gmail.com>
2024-01-24 20:57:14 +0600

    added profile

--- app/src/main/java/com/example/insectdetection/MainActivity.java
+++ app/src/main/java/com/example/insectdetection/MainActivity.java
@@ -1,9 +1,15 @@
 // MainActivity.java
 package com.example.insectdetection;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -25,8 +31,47 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottomNavView);
         frameLayout = findViewById(R.id.frameLayout);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if(itemId == R.id.navHome){
+
+                    loadFragment(new HomeFragment(),false);
+
+                } else if (itemId == R.id.navProfile) {
+
+                    loadFragment(new profileFragment(),false);
+
+                } else if (itemId == R.id.navRatings) {
+
+                    loadFragment(new ratingFragment(),false);
+
+                }
+
+                return  true;
+            }
+        });
+
+        loadFragment(new HomeFragment(),true);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new profileFragment()).commit();
         }
+    }
+    private  void loadFragment(Fragment fragment , boolean isAppInitialized){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (isAppInitialized){
+            fragmentTransaction.add(R.id.frameLayout,fragment);
+        }
+        else{
+            fragmentTransaction.replace(R.id.frameLayout,fragment);
+        }
+
+        fragmentTransaction.commit();
     }
 }
