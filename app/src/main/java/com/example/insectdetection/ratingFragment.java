3018a8be7c125e48113399efa807ee4796e7b8e3
fsananna <sultanafarhana55@gmail.com>
2024-02-21 12:38:57 +0600

    database problem solved

--- app/src/main/java/com/example/insectdetection/ratingFragment.java
+++ app/src/main/java/com/example/insectdetection/ratingFragment.java
@@ -7,12 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 public class ratingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rating, container, false);
+        View view = inflater.inflate(R.layout.fragment_rating, container, false);
+        WebView webView = view.findViewById(R.id.webView);
+        String video ="<iframe width=\"100%\" height=\"100%\" src=\"https://www.youtube.com/embed/Oo_IZysj4F4?si=XxLiqqxWi1GgYrtn\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" allowfullscreen></iframe>";
+        webView.loadData(video,"text/html","utf-8");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        return view;
     }
 }
